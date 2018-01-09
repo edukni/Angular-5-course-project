23a3d4066a683af84b4ca4971ee260004488d9a1
edukni <eduk_1012@hotmail.com>
2018-01-09 15:49:03 -0600

    Using Local References in Templates

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -9,20 +9,21 @@ export class CockpitComponent implements OnInit {
  @Output() serverCreated = new EventEmitter<{serverName:string, serverContent:string}>();
  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName:string, serverContent:string}>();;
   
-  newServerName = '';
+  // newServerName = '';
   newServerContent = '';
+  
   
   constructor() { }
 
   ngOnInit() {
   }
 
-  onAddServer() {
-    this.serverCreated.emit({serverName: this.newServerName, serverContent:this.newServerContent} );
+  onAddServer(nameInput:HTMLInputElement) {
+    this.serverCreated.emit({serverName: nameInput.value, serverContent:this.newServerContent} );
   }
 
-  onAddBlueprint() {
-    this.blueprintCreated.emit({serverName: this.newServerName, serverContent:this.newServerContent} );
+  onAddBlueprint(nameInput:HTMLInputElement) {
+    this.blueprintCreated.emit({serverName: nameInput.value, serverContent:this.newServerContent} );
     
   }
 
